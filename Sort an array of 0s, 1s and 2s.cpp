2cf9a1ca73024491e1f348171a472471fd138fb0
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-02-07 11:33:50 +0530

    Sort an array of 0s, 1s and 2s

---
+++ Sort an array of 0s, 1s and 2s.cpp
@@ -0,0 +1,9 @@
+Q.Sort an array of 0s, 1s and 2s
+link-->https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+code--> void sort012(int a[], int n)
+    {
+        // code here 
+        sort(a , a+n);
+    }
+Expected Time Complexity: O(Nlogn)
+Expected Auxiliary Space: O(1)
