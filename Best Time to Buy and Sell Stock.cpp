12fd51db1fee40c004677f2a232b21635d9d5a65
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-02-11 17:50:55 +0530

    Best Time to Buy and Sell Stock

---
+++ Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,13 @@
+Q.121. Best Time to Buy and Sell Stock
+link-->int maxProfit(vector<int>& prices) {
+        int maxprofit=0;
+        int minprice=INT_MAX;
+        for(int i=0;i<prices.size() ; i++){
+            if(prices[i]<minprice){
+                minprice=prices[i];
+                
+            }
+            maxprofit=max(maxprofit , prices[i]-minprice);
+        }
+        return maxprofit;
+    }
